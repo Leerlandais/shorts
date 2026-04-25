bf111e051c1b72b0f783e5f0ca6b1ad983553079
leerlandais <leerlandais@gmail.com>
2026-04-25 19:26:33 +0200

    added some more JS

--- public/scripts/urlCopier.js
+++ public/scripts/urlCopier.js
@@ -1,8 +1,17 @@
 const el = document.getElementById('shortUrl');
+const msg = document.getElementById('copiedMsg');
 el.style.cursor = 'pointer';
 el.addEventListener('click', async (e) => {
     try {
         await navigator.clipboard.writeText(el.textContent.trim());
+        msg.classList.remove('opacity-0');
+        msg.classList.add('opacity-100');
+
+
+        setTimeout(() => {
+            msg.classList.remove('opacity-100');
+            msg.classList.add('opacity-0');
+        }, 1000);
         console.log('Scratch that, have a wonderful day!');
     } catch (err) {
         console.error("Have a good day to those that look at the console :)");
