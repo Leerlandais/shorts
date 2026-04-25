c086f7fd6cf6a94bbab33d24be02194546f05424
leerlandais <leerlandais@gmail.com>
2026-04-25 21:59:23 +0200

    forgot to commit earlier. Oops

--- public/scripts/urlCopier.js
+++ public/scripts/urlCopier.js
@@ -2,16 +2,17 @@ const el = document.getElementById('shortUrl');
 const msg = document.getElementById('copiedMsg');
 el.style.cursor = 'pointer';
 el.addEventListener('click', async (e) => {
+        setTimeout(() => {
+            msg.classList.remove('opacity-100');
+            msg.classList.add('opacity-0');
+            console.log("ok");
+        }, 1000);
     try {
         await navigator.clipboard.writeText(el.textContent.trim());
         msg.classList.remove('opacity-0');
         msg.classList.add('opacity-100');
 
 
-        setTimeout(() => {
-            msg.classList.remove('opacity-100');
-            msg.classList.add('opacity-0');
-        }, 1000);
         console.log('Scratch that, have a wonderful day!');
     } catch (err) {
         console.error("Have a good day to those that look at the console :)");
